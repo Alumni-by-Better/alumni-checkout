4748600b9a46f131dccdbfa7acdc45bbdb3bd8d2
Michael Braga <michaelbraga.dev@gmail.com>
2024-09-13 17:54:45 -0300

    feat: configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: '/alumni-checkout/',
-  build: {
-    minify: true,
-    sourcemap: false,
-    target: 'modules',
-  },
+  plugins: [react()],
 })
